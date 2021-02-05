7ee1680f34828d0282d54e93428801027a559848
Oishi Akito <66ed3gs@gmail.com>
2021-02-05 11:59:29 +0900

    add(template): first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
